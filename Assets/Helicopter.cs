5853b672a7aff6fadee24fe75ce35170c2cf2550
Dino Cuturdic <dino@adenogame.com>
2023-02-14 12:20:35 +0100

    Added heli mats

---
+++ Assets/Helicopter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour
+{
+    [SerializeField] Transform topRotor, backRotor;
+
+    private AudioSource aud;
+    
+    private void Start() {
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void Update() {
+        topRotor.Rotate(Vector3.up, -10);
+        backRotor.Rotate(Vector3.forward, 10);
+    }
+}
